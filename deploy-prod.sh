c6ead138ffaf626f3177dbe6f8586bdee4dfe666
Ichsan Budiman <budimanichsan@gmail.com>
2025-10-03 10:35:50 +0100

    change docker compose command using new version

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -3,20 +3,20 @@
 echo "Starting production deployment..."
 
 echo "Stopping existing containers..."
-docker-compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml down
 
 
 echo "Building and starting production containers..."
-docker-compose -f docker-compose.prod.yml --env-file .env.production.server up -d --build
+docker compose -f docker-compose.prod.yml --env-file .env.production.server up -d --build
 
 echo "Waiting for database to be ready..."
 sleep 30
 
 echo "Running database migrations..."
-docker-compose -f docker-compose.prod.yml exec web npx prisma migrate deploy
+docker compose -f docker-compose.prod.yml exec web npx prisma migrate deploy
 
 echo "Checking container status..."
-docker-compose -f docker-compose.prod.yml ps
+docker compose -f docker-compose.prod.yml ps
 
 echo "Production deployment completed!"
 echo "Application will be available at: http://194.233.91.96:3212"
