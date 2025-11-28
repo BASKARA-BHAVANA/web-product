e1fd95fa76e062248e835ee5517715fb29b5c1a1
Diaz-Adrianz <diazz.developer@gmail.com>
2025-11-28 09:19:46 +0700

    style: faq

--- src/app/(app)/profil/unggah-krs/page.tsx
+++ src/app/(app)/profil/unggah-krs/page.tsx
@@ -7,6 +7,12 @@ import Headline from '@/components/molecules/headline';
 import React from 'react';
 import { uploadKrs } from '../actions';
 import { auth } from '@/lib/actions/auth';
+import {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionContent,
+} from '@/components/atoms/accordion';
 
 const Page = async () => {
   await auth();
@@ -20,7 +26,7 @@ const Page = async () => {
           headText="Unggah KRS"
         />
         <FlashActionResult />
-        <Card>
+        <Card className="mb-6">
           <CardContent>
             <form action={uploadKrs} className="flex gap-2">
               <Input name="krs" type="file" accept="application/pdf" />
@@ -28,6 +34,43 @@ const Page = async () => {
             </form>
           </CardContent>
         </Card>
+
+        <Accordion type="single" collapsible>
+          <AccordionItem value="faq-1">
+            <AccordionTrigger>
+              Mengapa saya perlu mengunggah KRS?
+            </AccordionTrigger>
+            <AccordionContent>
+              Pengunggahan KRS diperlukan agar Kamu dapat mengakses ke berbagai
+              konten yang dirancang secara eksklusif untuk kelompok mahasiswa
+              tertentu. Ini memastikan bahwa Kamu mendapatkan informasi dan
+              layanan yang paling relevan.
+            </AccordionContent>
+          </AccordionItem>
+
+          <AccordionItem value="faq-2">
+            <AccordionTrigger>
+              Seberapa aman KRS yang saya unggah?
+            </AccordionTrigger>
+            <AccordionContent>
+              Sistem hanya mengekstrak beberapa data yang diperlukan dari file
+              KRS. Sistem tidak mengambil keseluruhan isi-nya dan tidak
+              menyimpan file KRS Kamu setelah proses ekstraksi selesai.
+            </AccordionContent>
+          </AccordionItem>
+
+          <AccordionItem value="faq-3">
+            <AccordionTrigger>
+              Bagaimana prosedur jika terjadi kesalahan saat sistem mengekstrak
+              KRS?
+            </AccordionTrigger>
+            <AccordionContent>
+              Jika Kamu mendapati adanya ketidaksesuaian atau kesalahan dalam
+              ekstraksi data KRS, Silakan segera hubungi kontak Kabinet Himatif
+              yang aktif.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
       </Container>
     </>
   );
